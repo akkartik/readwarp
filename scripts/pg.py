308c589d5fbfaa9cabb353a544c88a661d9954be
Kartik K. Agaram <vc@akkartik.com>
2010-03-17 20:36:13 -0700

    668

--- scripts/pg.py
+++ scripts/pg.py
@@ -1,7 +1,13 @@
-import sys
+import sys, string
+import time
 from datetime import datetime
-import string
+import json
+from json_extensions import to_json
 
 for line in sys.stdin.readlines():
   doc, mon, yr = string.split(line)
-  print doc, datetime.strptime(mon+" "+yr, "%B %Y")
+  with open('urls/'+doc+'.metadata') as input:
+    f = json.load(input)
+  f['date'] = time.mktime(datetime.strptime(mon+" "+yr, "%B %Y").timetuple())
+  with open(doc+'.metadata', 'w') as output:
+    json.dump(f, output, default=to_json)
