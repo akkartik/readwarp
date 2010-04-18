26f44570c70bcef1d94627ec8c74c260e8b0075d
Kartik K. Agaram <vc@akkartik.com>
2010-04-18 14:46:46 -0700

    836

--- htmlclean.py
+++ htmlclean.py
@@ -1,12 +1,35 @@
 import sys, os, time, re, math, string, traceback, json, codecs
-from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulSoup, Tag, NavigableString
 from utils import *
 import difflib
 
 # http://code.google.com/p/arc90labs-readability/source/browse/tags/final-releases/0.4.5/js/readability.js
 
-badParaRegex = re.compile("comment|meta|footer|footnote")
-goodParaRegex = re.compile("^(post|hentry|entry[-]?(content|text|body)?|article[-]?(content|text|body)?)$")
+def cleanup(file):
+  deschint = hint_contents(file)
+  soup = preproc(file)
+
+  scores = {}
+  for para in soup.findAll('p'):
+    parent = para.parent
+    pars = unicode(parent)
+    if not scores.has_key(pars):
+      scores[pars] = init(parent)
+
+    scores[pars] += lenScore(para)
+    scores[pars] += commaCount(para)
+
+  candidates = sortedKeys(scores)
+  pick = pickTopMatchingCandidate(candidates, scores, htmlstrip(deschint))
+  if pick: return postproc(pick)
+
+  if deschint == '':
+    try: return postproc(candidates[0])
+    except: pass
+  return deschint
+
+badParaRegex = re.compile("comment|meta|footer|footnote", re.I)
+goodParaRegex = re.compile("^(post|hentry|entry[-]?(content|text|body)?|article[-]?(content|text|body)?)$", re.I)
 def init(node):
   ans = 0
   if node.has_key('class'):
@@ -73,37 +96,31 @@ def pickTopMatchingCandidate(candidates, scores, hint_stripped):
 
   return None
 
-def postproc(node):
-  if node is None: return
-  return re.sub(r"^<td ", "<div ", node)
-
-def cleanup(file):
-  deschint = hint_contents(file)
-  soup = BeautifulSoup(re.sub(r"<br\s*/?\s*>\s*<br\s*/?\s*>", "<p>", slurp(file)))
+def preproc(file):
+  soup = BeautifulSoup(re.sub(r'</?font[^>]*>', '',
+    re.sub(r"<br\s*/?\s*>\s*<br\s*/?\s*>", "<p>", slurp(file))))
 
   for s in soup.findAll('script'): s.extract()
   for s in soup.findAll('style'): s.extract()
   for s in soup.findAll('link', attrs={'type': 'text/css'}): s.extract()
+
   for s in soup.findAll('form'): s.extract()
+  for s in soup.findAll('object'): s.extract()
+  for s in soup.findAll('iframe'): s.extract()
 
-  scores = {}
-  for para in soup.findAll('p'):
-    parent = para.parent
-    pars = unicode(parent)
-    if not scores.has_key(pars):
-      scores[pars] = init(parent)
+  for s in soup.findAll('h1'): s.extract()
+  if len(soup.findAll('h2')) == 1:
+    for s in soup.findAll('h2'): s.extract()
 
-    scores[pars] += lenScore(para)
-    scores[pars] += commaCount(para)
+  for s in soup.findAll(True):
+    try: del(s['style'])
+    except KeyError: pass
 
-  candidates = sortedKeys(scores)
-  pick = pickTopMatchingCandidate(candidates, scores, htmlstrip(deschint))
-  if pick: return postproc(pick)
+  return soup
 
-  if deschint == '':
-    try: return postproc(candidates[0])
-    except: pass
-  return deschint
+def postproc(node):
+  if node is None: return
+  return re.sub(r"^<td ", "<div ", node)
 
 def commaCount(node):
   return len(node.renderContents().split(','))
