9cda8e12f028ac74f5a368a642ef04e895cd8463
Kartik K. Agaram <vc@akkartik.com>
2010-04-29 12:56:42 -0700

    894

--- x.js
+++ x.js
@@ -2,7 +2,6 @@ require.paths.unshift("/app/local/share/lib/js");
 var sys = require('sys');
 var fs = require('fs');
 var Haml = require('haml');
-var tw = require('./twitter');
 var db = require('./db');
 
 var f = function(a, b) {
@@ -11,7 +10,3 @@ var f = function(a, b) {
 
 sys.puts(Haml.render(fs.readFileSync('x.haml'),
                     {locals: {var1: 3, var2: 34, f: function(a, b){return a+b;} }}));
-
-tw.crawl_tweets('akkartik', function(elem) {
-  db.add('tweets', elem['id'], elem);
-});
